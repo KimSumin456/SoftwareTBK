c8c6fcc43e76a2302b40eb82699d3b2426a1fec5
KimSumin <ajou456@ajou.ac.kr>
2022-01-22 04:43:24 +0900

    FEAT: Implement selectedMenu
    It is a list of menus selected by the user. This is used to plan a timetable.

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
-var spicys = document.getElementsByClassName("spicy");
-for (var i = 0; i < spicys.length; i++)
+function showSpicyColor()
 {
-    var spicy = spicys.item(i).innerHTML[0];
-    spicyColor = "rgb(255, "+64*(5-spicy)+", 0)";
-    spicys.item(i).style.color = spicyColor;
+    var spicys = document.getElementsByClassName("spicy");
+    for (var i = 0; i < spicys.length; i++)
+    {
+        var spicy = spicys.item(i).innerText[1];
+        spicyColor = "rgb(255, "+64*(5-spicy)+", 0)";
+        spicys.item(i).style.color = spicyColor;
+    }
 }
 
-var levels = document.getElementsByClassName("level");
-for (var i = 0; i < levels.length; i++)
+var menuBoard =
 {
-    var level = levels.item(i).innerHTML[2]
-    levelColor = "rgb(255, "+64*(5-level)+", 0)";
-    levels.item(i).style.color = levelColor;
+    totalSpicy: 0,
+
+    selectedMenu: {},
+
+    onSelectMenu: function(self) {
+        if (self.checked)
+        {
+            this.selectedMenu[self.value] = self.nextElementSibling.innerText + self.nextElementSibling.nextElementSibling.innerText + " ";
+        }
+        else
+        {
+            delete this.selectedMenu[self.value];
+        }
+        
+        var selectedMenuString = "";
+        for (var key in this.selectedMenu)
+        {
+            selectedMenuString += this.selectedMenu[key];
+        }
+        document.getElementById("selectedMenu").innerText = selectedMenuString;
+        
+        console.log(this.selectedMenu);
+    }
 }
